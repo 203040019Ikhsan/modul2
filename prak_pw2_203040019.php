9b3482bed5d43ddd04458d5ecd2ad5a1ad38c24e
Ikhsan Ardiansyah <72891992+203040019Ikhsan@users.noreply.github.com>
2021-03-17 01:02:54 +0700

    Add files via upload

---
+++ prak_pw2_203040019.php
@@ -0,0 +1,62 @@
+<?php
+function gantiStyle($tulisan, $style1, $style2){
+    // isi  statement yang diperlukan di dalam sini
+    // tulisan28px,arial,berwarna#8c782d,italic dan border
+    // tambahkan border dan box shadow di div pembungkusnya
+}
+
+$jawabanIsset = "Isset adalah = . . . . . . . . . <br><br>";
+$jawabanEmpty = "Empty adalah = . . . . . . . . .";
+
+function soal($style){
+
+}
+
+function tumpukanbola($tumpukan) {
+
+}
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+    .bola {
+    }
+    </style>
+</head>
+<body>
+
+    <?php tumpukanbola(5) ?>
+</body>
+</html>
+
+function hitunganDeteminan(parameter) {
+    // baris code perhitungan determinan
+
+    // menampilkan matriks
+    echo"<table style='border-left: ...; border-right: ...;' cellspacing='5' cellpadding='5'>"
+    <tr>
+        <td>...</td>
+        <td>...</td>
+    </tr>
+    <tr>
+        <td>...</td>
+        <td>...</td>
+    </tr>
+    </table>";
+echo "...."; // menampilkan Teks dibawah matriks
+}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php hitungdeterminan(1,2,3,4) ?> <!-- Memanggil function hitunganDeterminan --!>
+</body>
+</html>
+
+?>
